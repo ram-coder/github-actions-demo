c9d00c6709f06aec1be2a646d782a0918fa654fa
ram-coder <56632550+ram-coder@users.noreply.github.com>
2025-06-26 21:43:54 +0100

    Create main.tf

---
+++ terraform/nsg-rule/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_network_security_group" "target_nsg" {
+  name                = "uksouth-nsg"
+  resource_group_name = "prod-uksouth-rg"
+}
+
+resource "azurerm_network_security_rule" "rdp_rule" {
+  name                        = "Allow-RDP-DynamicIP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = var.my_public_ip
+  destination_address_prefix  = "*"
+  network_security_group_name = data.azurerm_network_security_group.target_nsg.name
+  resource_group_name         = data.azurerm_network_security_group.target_nsg.resource_group_name
+}
