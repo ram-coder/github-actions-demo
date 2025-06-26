85c96a6095e06ee22cd6a579739aaf87507e011f
ram-coder <56632550+ram-coder@users.noreply.github.com>
2025-06-26 22:03:37 +0100

    Update variables.tf

--- terraform/nsg-rule/variables.tf
+++ terraform/nsg-rule/variables.tf
@@ -1,4 +1,20 @@
 variable "my_public_ip" {
-  description = "Public IP address to allow for RDP access"
   type        = string
+  description = "Public IP to allow for RDP"
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
 }
